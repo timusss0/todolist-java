7788085da749211e86c7d529ddb11216e57a7ce0
timusss0 <mustikavia86@gmail.com>
2023-08-13 19:16:48 +0700

    test showtodolist

--- src/appTodoList.java
+++ src/appTodoList.java
@@ -4,21 +4,31 @@ public class appTodoList {
     public static String[] model = new String[10];
 
     public static void main(String[] args) {
-
+        testShowTodoList();
     }
 //LOGIC
 //menampikan todo ke list
-    public static void showTodoList(){
+    public static void showTodoList() {
         for (var i = 0; i < model.length; i++) {
             var todo = model[i];
-            var no =  i + 1;
+            var no = i + 1;
 
 //            jika todo tidak sama dengan null
-            if(todo != null){
+            if (todo != null) {
                 System.out.println(no + ". " + todo);
             }
         }
+
     }
+
+    public static void testShowTodoList(){
+        model[0] = "belajar java dasar";
+        model[1] = "belajar iklas";
+        model[2] = "belajar tidur tepat waktu";
+        showTodoList();
+    }
+
+
 //menambahkan todo ke list
     public static void addTodoList(){
 
