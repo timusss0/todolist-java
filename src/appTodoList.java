a35c9dea957a5ccc080aa9ee2795b58fb3eb277a
timusss0 <mustikavia86@gmail.com>
2023-08-13 19:12:55 +0700

    menampilkan todo

--- src/appTodoList.java
+++ src/appTodoList.java
@@ -6,10 +6,18 @@ public class appTodoList {
     public static void main(String[] args) {
 
     }
-//MODELLLL
+//LOGIC
 //menampikan todo ke list
     public static void showTodoList(){
+        for (var i = 0; i < model.length; i++) {
+            var todo = model[i];
+            var no =  i + 1;
 
+//            jika todo tidak sama dengan null
+            if(todo != null){
+                System.out.println(no + ". " + todo);
+            }
+        }
     }
 //menambahkan todo ke list
     public static void addTodoList(){
