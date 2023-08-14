12ea989c4ce9a0a9f2a188f3dfe282863c0ab41c
timusss0 <mustikavia86@gmail.com>
2023-08-14 07:56:10 +0700

    testremove todolist

--- src/appTodoList.java
+++ src/appTodoList.java
@@ -4,9 +4,10 @@ public class appTodoList {
     public static String[] model = new String[10];
 
     public static void main(String[] args) {
-        testAddToDoList();
+        testRemoveTodoList();
     }
-//LOGIC
+
+    //LOGIC
 //menampikan todo ke list
     public static void showTodoList() {
         for (var i = 0; i < model.length; i++) {
@@ -21,7 +22,7 @@ public class appTodoList {
 
     }
 
-    public static void testShowTodoList(){
+    public static void testShowTodoList() {
         model[0] = "belajar java dasar";
         model[1] = "belajar iklas";
         model[2] = "belajar tidur tepat waktu";
@@ -29,15 +30,15 @@ public class appTodoList {
     }
 
 
-//menambahkan todo ke list
-    public static void addTodoList(String todo){
+    //menambahkan todo ke list
+    public static void addTodoList(String todo) {
 
 //        bagaimana jika array sudah penuh?
 //        cek sudah penuh atau belum
 //        apakah model penh?
         var isFull = true;
         for (int i = 0; i < model.length; i++) {
-            if(model[i] == null){
+            if (model[i] == null) {
 //                model masih ada yang kosong
                 isFull = false;
                 break;
@@ -45,7 +46,7 @@ public class appTodoList {
         }
 
 //        jika penuh kita resize ukuran array 2x lipat
-        if(isFull){
+        if (isFull) {
             var temp = model;
             model = new String[model.length * 2];
 
@@ -57,27 +58,56 @@ public class appTodoList {
 
 //    tambahkan ke posisi yang arraynya NULL
         for (int i = 0; i < model.length; i++) {
-            if(model[i] == null){
+            if (model[i] == null) {
                 model[i] = todo;
                 break;
             }
         }
     }
 
-//    TEST ADD TODO
-    public static void testAddToDoList(){
+    //    TEST ADD TODO
+    public static void testAddToDoList() {
         for (int i = 0; i < 25; i++) {
-            addTodoList("contoh todo ke "  + i);
+            addTodoList("contoh todo ke " + i);
         }
         showTodoList();
     }
 
 
-//menghapus todo di list
-    public static void removeToDoList(){
-
+    //menghapus todo di list
+    public static boolean removeToDoList(Integer number) {
+        if ((number - 1) >= model.length) {
+            return false;
+        } else if (model[number - 1] == null) {
+            return false;
+        } else {
+            for (int i = (number - 1); i < model.length; i++) {
+                if (i == (model.length - 1)) {
+                    model[i] = null;
+                } else {
+                    model[i] = model[i + 1];
+                }
+            }
+        }
+        return false;
     }
 
+
+//    buat testremove
+    public static void testRemoveTodoList(){
+        addTodoList("satu");
+        addTodoList("dua");
+        addTodoList("tiga");
+        addTodoList("Empat");
+        addTodoList("lima");
+
+        var result = removeToDoList(20);
+        System.out.println(result);
+
+        result = removeToDoList(2);
+        System.out.println(result);
+        showTodoList();
+    }
 
 
 //VIEWW
